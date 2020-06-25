77e342524752048288702155e56264696b954842
DanielFCoelho <daniel_freita@outlook.com>
2020-06-25 10:33:19 -0300

    Add files via upload

---
+++ PocAutoMapperObjectToList/MapperConfigurationAccessor.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PocAutoMapperObjectToList
+{
+    public class MapperConfigurationAccessor
+    {
+        public static MapperConfiguration Configuration => new MapperConfiguration(config =>
+        {
+            config.CreateMap<SceneAccessResponse, List<SceneAccess>>()
+            .ConvertUsing((source, destination) =>
+            {
+                destination = new List<SceneAccess>();
+                foreach (string users in source.ReleasedUsers)
+                {
+                    destination.Add(new SceneAccess() { ProgramId = source.ProgramId, SceneId = source.Id });
+                }
+
+                return destination;
+            });
+        });
+    }
+}
